f47f3b500c0ca9c70bcc190da9906b5e756b084a
Kennedy <tuks1032@gmail.com>
2025-03-17 21:58:26 +0200

    top half for mobile, tablet and desktop done

---
+++ ip_address_tracker/Components/Pages/Home.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ip_address_tracker.Components.Pages
+{
+    public partial class Home : ComponentBase
+    {
+        private string ip_address = string.Empty;
+
+    }
+}
