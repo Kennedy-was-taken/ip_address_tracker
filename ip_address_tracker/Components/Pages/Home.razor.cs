1825eaf5ce12c5b300c275f363043b87f717eeda
Kennedy <tuks1032@gmail.com>
2025-03-22 14:57:38 +0200

    third party map intergrated

--- ip_address_tracker/Components/Pages/Home.razor.cs
+++ ip_address_tracker/Components/Pages/Home.razor.cs
@@ -1,10 +1,65 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace ip_address_tracker.Components.Pages
 {
-    public partial class Home : ComponentBase
+    public partial class Home : ComponentBase, IAsyncDisposable
     {
+        private Lazy<IJSObjectReference> ExampleModule = new();
+
         private string ip_address = string.Empty;
 
+        private string api_key = "at_vaFBN1fKp81LTOLx8xs2Ix5V0OOJ1";
+        private string bare_url = "https://geo.ipify.org/api/v2/country,city?";
+
+        [Inject]
+        private IJSRuntime? JS { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+             string data;
+            Console.WriteLine("Life Life");
+
+            data = await Task.FromResult("Async data loaded");
+
+            Console.WriteLine();
+
+
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender && JS is not null)
+            {
+                ExampleModule = new(await JS.InvokeAsync<IJSObjectReference>("import", "./js/map_function.js"));
+
+                await ExampleModule.Value.InvokeVoidAsync("initMap");
+
+            }
+
+        }
+
+
+        public void HandleButtonClick()
+        {
+            Console.WriteLine("sfef");
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (ExampleModule.IsValueCreated)
+            {
+                try
+                {
+                    await ExampleModule.Value.DisposeAsync();
+                }
+
+                catch (Exception)
+                {
+
+                }
+                
+            }
+        }
     }
 }
---
+++ ip_address_tracker/wwwroot/js/map_function.js
@@ -0,0 +1,25 @@
+﻿
+
+export function initMap() {
+
+    coordinates("", "");
+}
+
+
+function coordinates(laxX, latY) {  
+
+    if ( (laxX == "" || latY == "") || (laxX == " " || latY == " ") ) {
+        var map = L.map('map').setView([51.505, -0.09], 13);
+    }
+
+    else {
+        var map = L.map('map').setView([laxX, latY], 13);
+    }
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> ' +
+            ' < a href="https://www.openstreetmap.org/fixthemap">report problems</a>'
+    }).addTo(map);
+}
+
